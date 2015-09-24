256eae15ea357eeb2ccc570ecc3cd0a05e8c6093
Charles Bloomfield <charles.bloomfield@mail.mcgill.ca>
2015-09-24 14:13:20 -0400

    initial hacker news script form done

---
+++ hacker.py
@@ -0,0 +1,113 @@
+###############################################
+### COMP 598 : Project #1                   ###
+### ---------------------					###
+### Authors:                                ###
+###  + Nicolas Angelard-Gontier - 260532513 ###
+###  + Genevieve Fried - #########          ###
+###  + Charlie Bloomfield - 260520615       ###
+###############################################
+
+import json
+import requests
+from collections import deque
+
+items = open("./data/items", "a")
+users = open("./data/users", "a")
+
+itemCache = {}
+userCache = {}
+# comments = open("./data/comments", "a")
+# jobs     = open("./data/jobs", "a")
+# pollopts = open("./data/pollopts", "a")
+# polls    = open("./data/polls", "a")
+# stories  = open("./data/stories", "a")
+# users    = open("./data/users", "a")
+
+
+def itemUri(id):
+	"""
+	@param id of a hacker-news item
+	@returns uri that represents the item to be pulled
+	"""
+	return "https://hacker-news.firebaseio.com/v0/item/" + str(id) + ".json"
+
+
+def userUri(id):
+	"""
+	@param id of hacker-news user
+	@returns uri that represents the user to be pulled
+	"""
+	return "https://hacker-news.firebaseio.com/v0/user/" + id + ".json"
+
+
+def recordItem(jsonItem):
+	"""
+	@param json item to be recorded
+	stores the item in the appropriate text file
+	"""
+	items.write(str(jsonItem))
+
+
+def recordUser(jsonUser):
+	"""
+	@param json item to be recorded
+	stores the item in the appropriate text file
+	"""
+	users.write(str(jsonUser))
+
+
+def getItem(id):
+	"""
+	@param id of item to be pulled from hacker-news
+	@returns json item associated with id
+	automatically stores the item in a file
+	"""
+	if not id in itemCache:
+		jsonItem = requests.get(itemUri(id)).json()
+		recordItem(jsonItem)
+		itemCache[id] = jsonItem
+	return itemCache[id]
+
+
+def getUser(id):
+	"""
+	@param id of user to be pulled from hacker-news
+	@returns json user associated with id
+	automatically stores the user in a file
+	"""
+	if not id in userCache:
+		jsonUser = requests.get(userUri(id)).json()
+		recordUser(jsonUser)
+		userCache[id] = jsonUser
+	return userCache[id]
+
+
+def recordStory(jsonItem):
+	"""
+	@param id of story item
+	The data of interest in this search is hacker-news stories.
+	Handle story finds all data relates to a story, including comments and user information,
+	"""
+	userId = jsonItem["by"]
+	getUser(userId)
+
+	commentIds = deque(jsonItem["kids"])
+
+	#while comments is not empty
+	while commentIds:
+		commentId = commentIds.popleft()
+		comment = getItem(commentId)
+		if "kids" in comment:
+			childCommentIds = comment["kids"]
+			commentIds.extend(childCommentIds)
+		if "by" in comment:
+			getUser(comment["by"])
+
+
+def main():
+	jsonStory = getItem(8863)
+	recordStory(jsonStory)
+
+
+if __name__ == "__main__":
+	main()
