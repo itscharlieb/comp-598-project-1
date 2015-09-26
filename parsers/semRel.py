3d20efdf6cc1514ccbb02b3a838bf3bf1a02c5db
Genevieve Fried <genevievepike@gmail.com>
2015-09-25 22:01:08 -0400

    halfway through, need to pull code

--- parsers/semRel.py
+++ parsers/semRel.py
@@ -1,5 +1,7 @@
 import logging
 from gensim import corpora, models, similarities
+from collections import defaultdict
+from pprint import pprint 
 
 
 """
@@ -8,3 +10,33 @@ from gensim import corpora, models, similarities
 - convert tokenized documents to vectors
 - perform some sort of indexing algorithm, lsi or tdidf
 """
+
+class Tokenize:
+
+	def __init__(self, URL, article_dictionary):
+		self.URL = URL
+		self.ad = article_dictionary
+	
+	def createDocuments(self):
+		ad = self.ad
+
+		for 
+
+
+
+
+	def basicParse(self, document):
+		#remove common words and tokenize
+		stoplist = set('for a of the and to in'.split())
+		texts = [[word for word in document.lower().split() if word not in stoplist] for document in documents]
+
+		#remove words that appear only once
+		for text in texts:
+			for token in text:
+				frequency[token] += 1
+
+		texts = [[token for token in text if frequency[token] > 1] for text in texts]
+
+
+
+
